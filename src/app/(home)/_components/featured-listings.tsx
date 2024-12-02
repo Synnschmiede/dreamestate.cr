7367ef76c9d45b85ffd6c980b94f8165157c4c1b
codemine24 <codemine24@gmail.com>
2024-12-02 21:48:32 +0600

    logo slider basic setup done.

--- src/app/(home)/_components/featured-listings.tsx
+++ src/app/(home)/_components/featured-listings.tsx
@@ -1,29 +1,51 @@
 "use client"
 
-// Import Swiper styles
 import 'swiper/css';
 import { Swiper, SwiperSlide } from 'swiper/react';
-import { A11y, Scrollbar, Navigation, Pagination } from 'swiper/modules';
+import { A11y, Autoplay, Scrollbar, Navigation, Pagination } from 'swiper/modules';
 
 import { Box } from '@mui/material';
 
 export const FeaturedListings = () => {
+    const logos = [
+        "/assets/home/brand_1.svg",
+        "/assets/home/brand_2.svg",
+        "/assets/home/brand_3.svg",
+        "/assets/home/brand_4.svg",
+        "/assets/home/brand_5.svg",
+        "/assets/home/brand_6.svg",
+    ];
     return (
         <Box>
             <Swiper
-                modules={[Navigation, Pagination, Scrollbar, A11y]}
+                loop
+                autoplay={{ delay: 3000, disableOnInteraction: false }}
+                modules={[Navigation, Pagination, Scrollbar, A11y, Autoplay]}
                 spaceBetween={50}
-                slidesPerView={3}
+                slidesPerView={5}
                 navigation
                 pagination={{ clickable: true }}
                 scrollbar={{ draggable: true }}
                 onSwiper={(swiper) => console.log(swiper)}
                 onSlideChange={() => console.log('slide change')}
             >
-                <SwiperSlide>Slide 1</SwiperSlide>
-                <SwiperSlide>Slide 2</SwiperSlide>
-                <SwiperSlide>Slide 3</SwiperSlide>
-                <SwiperSlide>Slide 4</SwiperSlide>
+                {logos.map((logo, index) => (
+                    <SwiperSlide key={index}>
+                        <Box
+                            component="img"
+                            src={logo}
+                            alt={`Brand ${index + 1}`}
+                            sx={{
+                                width: "100%",
+                                height: "auto",
+                                display: "block",
+                                margin: "0 auto",
+                                maxHeight: 60,
+                                objectFit: "contain",
+                            }}
+                        />
+                    </SwiperSlide>
+                ))}
             </Swiper>
         </Box>
     );
